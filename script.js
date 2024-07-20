04fcd8d7e9e692e58da75c1493b6c8067c5ab1a5
Victor Melo <contatomelo2@gmail.com>
2024-07-20 13:16:42 -0300

    update
    atualização da estruturação, estilização e do script

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 function toggleParagraph(id) {
     var para = document.getElementById(id);
     if (para.style.display === "none" || para.style.display === "") {
-      para.style.display = "block";
+        para.style.display = "block";
     } else {
-      para.style.display = "none";
+        para.style.display = "none";
     }
-  }
-  
-  let lastScrollTop = 0;
-  window.addEventListener("scroll", function () {
+}
+
+let lastScrollTop = 0;
+window.addEventListener("scroll", function () {
     let navbar = document.getElementById("navbar");
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
-      navbar.style.top = "-60px";
+        navbar.style.top = "-60px";
     } else {
-      navbar.style.top = "0";
+        navbar.style.top = "0";
     }
     lastScrollTop = scrollTop;
-  
+
     let scrollToTop = document.getElementById("scrollToTop");
     if (scrollTop > 200) {
-      scrollToTop.style.display = "block";
+        scrollToTop.style.display = "block";
     } else {
-      scrollToTop.style.display = "none";
+        scrollToTop.style.display = "none";
     }
-  });
-  
-  function toggleMenu() {
+});
+
+function toggleMenu() {
     var x = document.getElementById("navLinks");
     if (x.style.display === "block") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
+        x.style.display = "block";
     }
-  }
-  
-  document.getElementById("scrollToTop").addEventListener("click", function () {
+}
+
+document.getElementById("scrollToTop").addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
-  });
+});
